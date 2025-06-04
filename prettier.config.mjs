3cee439df4f53e22f97d833bb63c29f30508bc8b
Sreekanth Nagareddy <sreeisalso@gmail.com>
2025-06-04 14:01:16 +0530

    prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+	trailingComma: "es5",
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+};
+
+export default config;
